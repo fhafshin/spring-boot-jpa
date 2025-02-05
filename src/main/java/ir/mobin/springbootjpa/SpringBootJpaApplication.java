009838b05ff1d572bb44e291cac81495637cde6e
fhafshin <fhafshin@gmail.com>
2025-02-05 12:55:51 +0330

    create h2 console

--- src/main/java/ir/mobin/springbootjpa/SpringBootJpaApplication.java
+++ src/main/java/ir/mobin/springbootjpa/SpringBootJpaApplication.java
@@ -1,13 +1,26 @@
 package ir.mobin.springbootjpa;
 
+import ir.mobin.springbootjpa.domain.Post;
+import ir.mobin.springbootjpa.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootJpaApplication {
+public class SpringBootJpaApplication implements CommandLineRunner {
 
+
+	private final PostRepository postRepository;
+	public SpringBootJpaApplication(PostRepository postRepository) {
+		this.postRepository = postRepository;
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJpaApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		postRepository.save(new Post("book"));
+	}
 }
---
+++ src/main/java/ir/mobin/springbootjpa/domain/Post.java
@@ -0,0 +1,36 @@
+package ir.mobin.springbootjpa.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="post")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String title;
+
+    public Post(String title) {
+        this.title = title;
+    }
+
+    public Post() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
